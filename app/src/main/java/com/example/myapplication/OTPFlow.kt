64c622c734ca2264a9b4d844c0364a54aadd94e1
Typoo06 <ngantruong1985@gmail.com>
2025-11-11 01:12:21 +0700

    OTP Verification Feature UI
    Feat: Implement single OTP verification screen for multiple flows (Signup, Forgot Password)

---
+++ app/src/main/java/com/example/myapplication/OTPFlow.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+object OtpFlow {
+    const val FLOW_TYPE = "OTP_FLOW_TYPE"
+    const val REGISTER = "REGISTER"
+    const val FORGOT_PASSWORD = "FORGOT_PASSWORD"
+}
